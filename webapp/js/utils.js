3c37d9090910b23ec15218682275fc7ecf8c5fb4
gavin <gavin.gao@e3-expo.com>
2017-11-08 11:47:45 +0800

    update utils

---
+++ webapp/js/utils.js
@@ -0,0 +1,12 @@
+'use strict'
+class UtilsMethod {
+  constructor () {
+    this.visions = '0.0.1'
+  }
+  getVisions () {
+    return this.visions
+  }
+}
+/* eslint-disable */
+const Utils = new UtilsMethod()
+/* eslint-enable */
