9761e618b6f14642403f45ac2a83cc49979d97cd
XiaoMuCOOL <895355044@qq.com>
2018-01-29 15:36:50 +0800

    add cdn

--- server/config/config.js
+++ server/config/config.js
@@ -17,8 +17,8 @@ const config = {
     pass: ''
   },
   github: {
-    clientID: 'ef17fd53612fb8bea361',
-    clientSecret: '5dc4ee2d35feac53a4e0d03e04179b24db1704ec',
+    clientID: '',
+    clientSecret: '',
     callbackURL: 'http://localhost:3000/auth/github/callback'
   },
   redis: {
